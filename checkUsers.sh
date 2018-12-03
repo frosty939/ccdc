b03fb2cafcf38aaec1ac466a3f22753aaee66bd2
Frosty939 <swfrosty@gmail.com>
2018-12-03 17:26:11 -0500

    0.7

--- checkUsers.sh
+++ checkUsers.sh
@@ -2,8 +2,8 @@
 #=======================================================================================
 #=== DESCIPTION ========================================================================
 	## check what users exist, logs them, then prints any non-standard users
-########################################################################################
-########################################################################################
+#=======================================================================================
+#=======================================================================================
 	#
 	#*************** NEED TO DO/ADD ***********************
 	# check /etc/profile (see what its doing when making a new profile)
@@ -22,59 +22,74 @@
 	# Per user subordinate group IDs.		/etc/subgid
 	# Per user subordinate user IDs.		/etc/subuid
 	# Shadow password suite configuration.	/etc/login.defs
-
+	#
 	#******************************************************
 	#
 #///////////////////////////////////////////////////////////////////////////////////////
 #|||||||||||||||||||||||| Script Stuff Starts |||||||||||||||||||||||||||||||||||||||||
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-#
+#### RUN function #####
+#######################
+function main(){	###
+	buildEmUp		###
+	whoDat			###
+	breakEmDown		###
+}					###
+#######################
+#------ error handling ----------
+### If error, give up			#
+#set -e							#
+#- - - - - - - - - - - - - - - -#
+### if error, do THING			#
+# makes trap global 			#
+# (works in functions)			#
+#set -o errtrace				#
+# 'exit' can be a func or cmd	#
+#trap 'exit' ERR				#
+#--------------------------------
 #### global backup var ####
-backupDir=$HOME"/ccdc_backups/$(basename "$0" | tr -d ".sh")"
+backupDir="$HOME""/ccdc_backups/$(basename "$0" | sed 's/\.sh$//')"
 ###########################################################################################
 # builds the 2 files for diff to check and copies them to a backup archive
 ###########################################################################################
-buildEmUp(){
+function buildEmUp(){
 	defList="root daemon bin sys sync games man lp mail news uucp proxy www-data backup list irc gnats nobody systemd-network systemd-resolve syslog messagebus _apt lxd uuidd dnsmasq landscape pollinate sshd"
 	defListPath="$backupDir""/defList.bak"
 	testList="$(compgen -u)"
 	testListPath="$backupDir""/testList"
 	# creating the dir if it doesn't exist
 	if [ ! -d $backupDir ]; then
+		printf "\n\n dir name ["$backupDir"]\n\n"
 		command mkdir -p "$backupDir"
+		echo "creating ccdc backup dir"
 	fi
 	# creating backups
-	command cp -a /etc/passwd $backupDir/origPasswd.bak
+	command cp -a /etc/passwd "$backupDir"/origPasswd.bak
 	# building files
-	echo $defList > $defListPath
-	echo $testList > $testListPath
+	echo $defList > "$defListPath"
+	echo $testList > "$testListPath"
 	echo ""
-	command sed -i 's/ /\n/g' $defListPath
-	command sed -i 's/ /\n/g' $testListPath
-
-	}
+	command sed -i 's/ /\n/g' "$defListPath"
+	command sed -i 's/ /\n/g' "$testListPath"
+}
 ###########################################################################################
 # comparing defined list and generated list
 ###########################################################################################
-whoDat(){
+function whoDat(){
 	#### comparing lists ############################
 	printf "\n----- Non-Standard Users on this Box -----\n"
-	command diff $defListPath $testListPath | grep ">"
-	printf "\n====== original files backed up to $backupDir--$(date +"%Y-%m-%d_%H%M") ======\n"
-	}
+	command diff "$defListPath" "$testListPath" | grep ">"
+	printf "\n====== original files backed up to "$backupDir"--$(date +"%Y-%m-%d_%H%M") ======\n"
+}
 ###########################################################################################
 # zipping and removing old files
 ###########################################################################################
-breakEmDown(){
-	command tar -zcf $HOME/ccdc_backups/$(basename "$0" | sed 's/\.sh//')--$(date +"%Y-%m-%d_%H%M").tar.gz -C $HOME/ccdc_backups $(basename "$0" | sed 's/\.sh//')
-	command rm -rf $backupDir
+function breakEmDown(){
+	command tar -zcf "$HOME"/ccdc_backups/$(basename "$0" | sed 's/\.sh//')--$(date +"%Y-%m-%d_%H%M").tar.gz -C "$HOME"/ccdc_backups $(basename "$0" | sed 's/\.sh//')
+	command rm -rf "$backupDir"
 }
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #+++++++++++++++++++++++++++++++++ FIGHT!! +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-main(){
-	buildEmUp
-	whoDat
-	breakEmDown
-	}
+
 main

